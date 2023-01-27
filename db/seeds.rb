16115ca1c91623c7c78a0c5d6dcccafb0d459c7c
unknown <senthilkumarbca>
2023-01-27 17:58:03 +0530

    faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating users"
+
+30.times do |u|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    contact: Faker::PhoneNumber.phone_number_with_country_code
+  )
+
+  puts "user #{u} has been created"
+end
